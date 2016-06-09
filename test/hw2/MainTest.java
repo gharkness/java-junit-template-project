9dfb239ded06b0ec1a958cf02a22a70e9c1ca172
Garrett Harkness <gharkness@users.noreply.github.com>
2016-06-08 22:26:08 -0500

    Create MainTest.java

---
+++ test/hw2/MainTest.java
@@ -0,0 +1,16 @@
+package hw2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import edu.iastate.cs228.Main;
+
+public class MainTest {
+	
+	@Test
+	public void Main(){
+		assertEquals("test", System.out.println("test"));
+	}
+	
+}
